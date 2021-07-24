f80f12e84f605434afea813bbb7e5dbc181603cd
vamshisai98 <saivamshianirudh136@gmail.com>
2021-07-24 20:52:27 +0530

    final design complete

--- src/app/components/dashboards/dashboard.component.ts
+++ src/app/components/dashboards/dashboard.component.ts
@@ -6,7 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  cards = [
+    {
+      img: 'Get Paid Instantly.png',
+      title: 'Quick cash disbursement',
+      label: 'Get terms loanas that your business needs in 72hrs',
+    },
+    {
+      img: 'Low interest rate.png',
+      title: 'Low-interest rate',
+      label:
+        'Achieve your financial goals with an amazing interest rate starting at 13% per annum',
+    },
+    {
+      img: 'Secure Payments.png',
+      title: 'Zero Paperwork',
+      label:
+        'Get started instantly by submitting only your basic details & bank statements',
+    },
+    {
+      img: 'freelancer_feature_icon_6@1.5x.png',
+      title: 'Ace your business finances',
+      label:
+        'Zero EMI for first 3 months on Back-to-Business loans of upto 1 lakh',
+    },
+    {
+      img: 'Covid.png',
+      title: 'Loans to fight COVID-19',
+      label: 'Get terms loanas that your business needs in 72hrs',
+    },
+  ];
   constructor() {}
-
   ngOnInit(): void {}
 }
