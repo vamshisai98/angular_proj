9d74821fbb60ef764d39c86c4797b4decf9a6309
vamshisai98 <saivamshianirudh136@gmail.com>
2021-07-25 11:00:21 +0530

    navbar on hover and sroll fix

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  navbarfixed: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @HostListener('window:scroll', ['$event']) onscroll() {
+    if (window.scrollY > 100) {
+      this.navbarfixed = true;
+    } else {
+      this.navbarfixed = false;
+    }
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
